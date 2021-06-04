5c0349d396ea0ddafd3cbd97d7932ac3c64ba5ca
Eugene Lapeko <eugene@lapeko.info>
2021-06-04 18:54:07 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,6 +5,28 @@ node default {
   }
   
   package { 'mysql':
+    ensure => absent
+  }
+  
+  include dev_user
+  include dev_editor
+}
+
+class dev_user{
+  user { 'eugene':
+    ensure => present,
+    managehome => true,
+    group => ['wheel'],
+    password => '$1$pWicQEb0$lGXc.RyHF7VAG7tKOpIap1'
+  }
+}
+
+class dev_editor{
+  package { 'vim':
     ensure => present
+  }
+  file { '/home/eugene/.vimrc':
+    ensure => file,
+    source => 'puppet:///modules/dev_environment/vimrc'
   }
 }
