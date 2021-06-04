6bb1d65643e9f8f019342245d94aa0c14df49eee
Eugene Lapeko <eugene@lapeko.info>
2021-06-04 21:12:51 +0300

    Update init.pp

--- site/dynamic/manifests/init.pp
+++ site/dynamic/manifests/init.pp
@@ -14,10 +14,11 @@ class dynamic {
   
   file { '/etc/httpd/conf.d/001-demosite.conf':
     ensure => file,
-    source => 'puppet:///modules/dynamic/001-demosite.conf'
+    source => 'puppet:///modules/dynamic/001-demosite.conf',
+    notify => Service['httpd']
   }
   
   service { 'httpd':
-     ensure => restarted
+     ensure => running
   }
 }
