0f5d9f296ed9d237e2896a5708f36ef67125de56
Eugene Lapeko <eugene@lapeko.info>
2021-06-04 21:01:27 +0300

    Create init.pp

---
+++ site/dynamic/manifests/init.pp
@@ -0,0 +1,23 @@
+class dynamic {
+  package { ['httpd', 'php']:
+    ensure => isntalled
+  }
+  
+  file { '/var/www/php':
+    ensure => directory
+  }
+  
+  file { '/var/www/php/index.php':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+  }
+  
+  file { '/etc/httpd/conf.d/001-demosite.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/001-demosite.conf'
+  }
+  
+  service { 'httpd':
+     ensure => restarted
+  }
+}
